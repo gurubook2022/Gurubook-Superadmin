e85cec87542a646acfef1838d31418be35ef347f
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2025-11-14 16:59:33 +0500

    fix: change page component to synchronous function

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { Title } from "@/components/ui/text";
 import Form from "./_components/form";
-const page = async () => {
+const page = () => {
   return (
     <div className="w-full space-y-4 sm:space-y-8">
       <Title className="text-center">Sign in to your Account</Title>
